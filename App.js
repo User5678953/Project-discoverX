847f7f072d1577bb9124304bd0c05b2eb557db21
User5678953 <kendynwebster@gmail.com>
2024-08-16 09:44:21 -0500

    Initial commit - Set up React app with Tailwind CSS

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css'; // Import Tailwind CSS
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Welcome to DiscoverX</h1>
+    </div>
+  );
+}
+
+export default App;
